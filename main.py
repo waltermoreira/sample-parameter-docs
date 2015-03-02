685d7e1e95c532c9b77cd38e2e42a3355e1b6247
Walter Moreira <walter@waltermoreira.net>
2015-03-02 14:58:11 -0600

    foo

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ def search(args):
     x = args['x']
     y = args['y']
     assert isinstance(x, basestring)
-    assert isinstance(y, basestring)
+    assert isinstance(y, int)
     print json.dumps('x = {}'.format(x))
     print '---'
     print json.dumps('y = {}'.format(y))
