f54b5611be763f2525440c2bf660f663e58474d0
Walter Moreira <walter@waltermoreira.net>
2015-03-04 11:46:25 -0600

    empty

--- main.py
+++ main.py
@@ -1,14 +1,23 @@
 import json
+from collections import Sequence
 
 
 def search(args):
     x = args['x']
     y = args['y']
+    z = args['z']
+    w = args['w']
     assert isinstance(x, basestring)
     assert isinstance(y, int)
+    assert isinstance(z, Sequence)
+    assert w in ('Foo', 'Bar')
     print json.dumps(args)
     print '---'
     print json.dumps('x = {}'.format(x))
     print '---'
     print json.dumps('y = {}'.format(y))
+    print '---'
+    print json.dumps('z = {}'.format(z))
+    print '---'
+    print json.dumps('w = {}'.format(w))
 
